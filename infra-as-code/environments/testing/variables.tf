ed713f58f5dacd6652b8585c9eedb2e5ef351b7f
Rafa Adorna <rafael.adorna@gmail.com>
2022-10-14 16:26:38 +0200

    Init terra files

---
+++ infra-as-code/environments/testing/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  type        = string
+  description = "Currently mono region. Region where to deploy."
+  default     = "eu-west-2"
+}
+
+variable "name" {
+  type        = string
+  description = "Suffix name for all the entities to create."
+  default     = "yougov-test"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment we are at."
+  default     = "testing"
+}
