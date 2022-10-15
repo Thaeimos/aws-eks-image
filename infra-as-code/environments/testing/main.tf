d80783a1c50fefd2be80b49288a988be007d677f
Rafa Adorna <rafael.adorna@gmail.com>
2022-10-15 09:18:29 +0200

    Dynamic subnets range creation

--- infra-as-code/environments/testing/main.tf
+++ infra-as-code/environments/testing/main.tf
@@ -3,15 +3,16 @@ data "aws_availability_zones" "azs" {
   state = "available"
 }
 
-locals {
-  az_names = data.aws_availability_zones.azs.names
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
 }
 
 locals {
   cluster_name = "eks-${random_string.suffix.result}"
-}
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
+  subnet_size = length(data.aws_availability_zones.azs.names)
+  subnet_numbers = range(0, local.subnet_size)
+  public_subnets_cidr = [ for subnet_number in local.subnet_numbers : cidrsubnet(var.main_cidr_block, 8, subnet_number) ]
+  private_subnets_cidr = [ for subnet_number in local.subnet_numbers : cidrsubnet(var.main_cidr_block, 8, (subnet_number + local.subnet_size)) ]
 }
